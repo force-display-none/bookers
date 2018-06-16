2671e43e6d4b18d31232f59f122b6597f79befab
kckw0917 <kckw0917@gmail.com>
2018-06-16 22:02:56 +0900

    空欄の時投稿弾く実装前です

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,12 @@
 class BooksController < ApplicationController
-  def index
-  	  @books = Book.all
+
+  def top
   end
 
-  def show
-  	  @book = Book.find(params[:id])
+  def index
+  	  @books = Book.all.order(created_at: :desc)
+      @book = Book.new
+
   end
 
   def new
@@ -12,23 +14,38 @@ class BooksController < ApplicationController
   end
 
   def create
+      @books = Book.all.order(created_at: :desc)
   	  book = Book.new(book_params)
-  	  book.save
-  	  redirect_to book_path
+  	  if book.save
+      redirect_to book_path(book), notice: "Book was successfully created."
+      else
+      redirect_to books_path
+      end
+  end
+
+  def show
+      @book = Book.find(params[:id])
   end
 
   def edit
-  	  @book = Book.find(params[:id]
+  	  @book = Book.find(params[:id])
   end
 
   def update
   	  book = Book.find(params[:id])
   	  book.update(book_params)
-  	  redirect_to book_path(book)
+  	  redirect_to book_path(book), notice: "Book was successfully updated."
+  end
+
+  def destroy
+      book = Book.find(params[:id])
+      book.destroy
+      redirect_to books_path, notice: "Book was successfully destroyed."
   end
 
   private
   def book_params
   	  params.require(:book).permit(:title, :body)
   end
+
 end
